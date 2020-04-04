72d0c5aca346689c5687ed9b1dc00d2f2ea3546b
Brian Heise <bnheise@gmail.com>
2020-04-04 10:20:19 +0900

    made POST_CREATED action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,19 @@
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const FETCH_POST = 'FETCH_POST';
+export const POST_CREATED = 'POST_CREATED';
+
+export function createPost(body) {
+  const request = fetch('http://reduxblog.herokuapp.com/api/?key=bri-post', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json/' },
+    body: JSON.stringify(body)
+  }).then(response => response.json());
+
+  return {
+    type: POST_CREATED,
+    payload: request
+  };
+}
 
 export function fetchPost(id) {
   const promise = fetch(`http://reduxblog.herokuapp.com/api/${id}?key=bri-post`)

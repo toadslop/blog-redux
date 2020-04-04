b4ac56db309d3cabf9eeab40b55722daa0bef0da
Brian Heise <bnheise@gmail.com>
2020-04-04 10:26:22 +0900

    made skeleton post new page

---
+++ src/containers/PostNew.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+
+class PostNew extends Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <input />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default PostNew;
